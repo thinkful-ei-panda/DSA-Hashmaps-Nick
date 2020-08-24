94e9b2d2aee8ef54aeb5612174f9dcd25bb9703b
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-24 11:58:20 -0500

    Q2 complete

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -27,6 +27,24 @@ function main() {
   // After entering 9 entries the capacity of the hashTable is 24 because
   // when we passed the MAX_LOAD_RATIO the capacity (8) was multiplied by
   // the SIZE_RATIO (3).
+
+  WhatDoesThisDo();
 }
+
+const WhatDoesThisDo = function () {
+  let str1 = 'Hello World.';
+  let str2 = 'Hello World.';
+  let map1 = new HashMap();
+  map1.set(str1, 10);
+  map1.set(str2, 20);
+  let map2 = new HashMap();
+  let str3 = str1;
+  let str4 = str2;
+  map2.set(str3, 20);
+  map2.set(str4, 10);
+
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+};
 
 main();
